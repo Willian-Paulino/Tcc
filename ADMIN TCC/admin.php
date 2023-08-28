fa60fd402ba9ec97df65a7f294772ac4194a51a3
Willian-Paulino <willianpaulinopires@gmail.com>
2023-08-27 22:18:18 -0300

    Organização dos arquivos

---
+++ ADMIN TCC/admin.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/style/style.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+  <title>Document</title>
+</head>
+<body>
+  <div class="container-fluid ">
+    <div class="row">
+      <div class="col-4 d-flex justify-content-center align-items-center menu">
+        <div class="menu__logo">
+          <img src="imgs/Untitled (2).png" class="menu__img">
+        </div>
+        <div class="menu__options">
+        <div class="options__estatisticas">
+          <p class="options__indicadores"><span class="material-symbols-outlined">
+bar_chart
+</span> Estatísticas do site</p>
+          <p class="options__acessos"><span class="material-symbols-outlined">
+trending_up
+</span>Gráfico de vendas</p>
+          <p class="options__acessos"><span class="material-symbols-outlined">
+timeline
+</span>Gráfico de acessos</p>
+        </div>
+        </div>
+      <div class="options__editar">
+        <p class="options__indicadores"><span class="material-symbols-outlined">
+edit
+</span>Editar</h1>
+        <p class="options__acessos"><span class="material-symbols-outlined">
+styler
+</span>Editar produtos</p>
+      <p class="options__acessos"><span class="material-symbols-sharp">
+photo
+</span>Editar slideshow</p>
+      </div>
+      </div>  
+    </div>
+  </div>
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</html>
